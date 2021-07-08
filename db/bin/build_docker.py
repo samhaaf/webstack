3ca8f6b9d9ef45de91fc909c9e26f4eb73005c79
samhaaf <samhaaf@gmail.com>
2021-07-08 15:35:59 -0700

    db: make commands for local docker image

---
+++ db/bin/build_docker.py
@@ -0,0 +1,51 @@
+
+import argparse
+import os, subprocess
+import json
+from pprint import pprint
+
+
+## Parse command line args
+parser = argparse.ArgumentParser()
+parser.add_argument('--stage', '-s', type=str, default=None)
+parser.add_argument('--port', '-p', type=int, default=5432)
+args = parser.parse_args()
+
+
+## assert stage argument is set
+assert args.stage is not None, '--stage argument expected'
+
+
+## Load config
+local_dir = os.path.dirname(os.path.realpath(__file__))
+os.chdir(f"{local_dir}/../../config/")
+process = subprocess.run(
+    ['make','print', f'stage={args.stage}'],
+    stdout=subprocess.PIPE,
+    stderr=subprocess.PIPE
+)
+if process.stderr:
+    raise RuntimeError(f'In config/bin/generate.py:\n\n{process.stderr.decode()}')
+try:
+    config = json.loads('\n'.join(process.stdout.decode().strip().split('\n')))
+except json.decoder.JSONDecodeError:
+    config = json.loads('\n'.join(process.stdout.decode().strip().split('\n')[1:-1]))
+
+
+## Init docker image
+process = subprocess.run(
+    [   'sudo', 'docker', 'create',
+        '--name', f"{config['project']['name']}-postgres-{args.stage}",
+        '--env', f"POSTGRES_DB={config['database']['name']}",
+        '--env', f"POSTGRES_USER={config['database']['username']}",
+        '--env', f"POSTGRES_PASSWORD={config['database']['password']}",
+        '-p', f'127.0.0.1:{args.port}:5432/tcp',
+        'postgres'
+    ],
+    stdout=subprocess.PIPE,
+    stderr=subprocess.PIPE
+)
+if process.stderr:
+    raise RuntimeError(
+        f'Error starting docker in start_image.py:\n\n{process.stderr.decode()}'
+    )
---
+++ db/bin/clear_docker.py
@@ -0,0 +1,45 @@
+
+import argparse
+import os, subprocess
+import json
+from pprint import pprint
+
+
+## Parse command line args
+parser = argparse.ArgumentParser()
+parser.add_argument('--stage', '-s', type=str, default=None)
+args = parser.parse_args()
+
+
+## assert stage argument is set
+assert args.stage is not None, '--stage argument expected'
+
+
+## Load config
+local_dir = os.path.dirname(os.path.realpath(__file__))
+os.chdir(f"{local_dir}/../../config/")
+process = subprocess.run(
+    ['make','print', f'stage={args.stage}'],
+    stdout=subprocess.PIPE,
+    stderr=subprocess.PIPE
+)
+if process.stderr:
+    raise RuntimeError(f'In config/bin/generate.py:\n\n{process.stderr.decode()}')
+try:
+    config = json.loads('\n'.join(process.stdout.decode().strip().split('\n')))
+except json.decoder.JSONDecodeError:
+    config = json.loads('\n'.join(process.stdout.decode().strip().split('\n')[1:-1]))
+
+
+## Init docker image
+process = subprocess.run(
+    [   'sudo', 'docker', 'rm',
+        f"{config['project']['name']}-postgres-{args.stage}"
+    ],
+    stdout=subprocess.PIPE,
+    stderr=subprocess.PIPE
+)
+if process.stderr:
+    raise RuntimeError(
+        f'Error starting docker in start_image.py:\n\n{process.stderr.decode()}'
+    )
